419e2218a35098d8f9e9a32406b11a65959de9fc
athul jain <123951017+athuljain@users.noreply.github.com>
2023-10-16 08:47:00 +0530

    first

---
+++ server.js
@@ -0,0 +1,34 @@
+//const express = require('express')
+// if change to import statement
+// package.json - edit-> type to module
+
+import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+import { config } from "dotenv";
+
+const app = express();
+
+// app middlewares
+
+app.use(morgan("tiny"));
+app.use(cors());
+app.use(express.json());
+config();
+
+// application port
+const port = process.env.PORT || 8080;
+
+// routes
+
+app.get("/", (req, res) => {
+  try {
+    res.json("Get Requset");
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`sever connected http://localhost:${port}`);
+});
